7b32d4c915e7ef14be7a9984be370a8c60c7c1cc
cm-kojimat <kojima.takashi@classmethod.jp>
2020-06-30 09:56:58 +0900

    rename function

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,9 @@
-const mod = require("../index");
+const add = require("../index");
 
 test("1+2=3", () => {
-  expect(mod(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
+});
+
+test("0+-2=-2", () => {
+  expect(add(0, -2)).toBe(-2);
 });
